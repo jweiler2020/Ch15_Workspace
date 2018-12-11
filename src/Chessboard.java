1e3015126e2d4dbd006282a5eb44047f73ef8c2f
jweiler2020 <jweiler2020@cusd.claremont.edu>
2018-12-11 07:39:27 -0800

    Chessboard.java

--- src/Chessboard.java
+++ src/Chessboard.java
@@ -23,11 +23,76 @@ public class Chessboard extends JFrame
 	
 	private void fillBoard()
 	{
+		/*
 		int[] arr = {4, 0, 7, 3, 1, 6, 2, 5};
 		for(int i = 0; i < 8; i++)
 		{
 			board[i][arr[i]].setFilled(true);
 		}
+		*/
+		addQueens(new int[][]{{4, 3}}, 1);
+	}
+	
+	// Board is a list of queen positions
+	private void addQueens(int[][] board, int numQueens)
+	{
+		boolean[][] protect = new boolean[8][8];
+		for(int n = 0; n < numQueens; n++)
+		{
+			protect = fillRow(protect, board[n][1]);
+			protect = fillColumn(protect, board[n][0]);
+			protect = fillDiagDown(protect, board[n][1], board[n][0]);
+		}
+		
+		
+		
+		
+		for(int i = 0; i < 8; i++)
+		{
+			for(int j = 0; j < 8; j++)
+			{
+				this.board[i][j].setFilled(protect[i][j]);
+			}
+		}
+	}
+	
+	private boolean[][] fillRow(boolean[][] b, int row)
+	{
+		for(int j = 0; j < b[0].length; j++)
+		{
+			b[row][j] = true;
+		}
+		return b;
+	}
+	
+	private boolean[][] fillColumn(boolean[][] b, int col)
+	{
+		for(int i = 0; i < b.length; i++)
+		{
+			b[i][col] = true;
+		}
+		return b;
+	}
+	
+	private boolean[][] fillDiagDown(boolean[][] b, int row, int col)
+	{
+		if (row > col)
+		{
+			int offset = row - col;
+			for(int i = row; i < 8; i++)
+			{
+				b[i][i-offset] = true;
+			}
+		}
+		else
+		{
+			int offset = col - row;
+			for(int i = col; i < 8; i++)
+			{
+				b[i-offset][i] = true;
+			}
+		}
+		return b;
 	}
 	
 	public void paint(Graphics g)
